436381869f6150cb485453a3d115569d5d7170a3
misterbabu281@gmail.com <misterbabu281@gmail.com>
2024-12-20 22:27:45 +0530

    first commit

---
+++ src/main/java/com/example/Jocata/DemoMethod.java
@@ -0,0 +1,12 @@
+package com.example.Jocata;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoMethod {
+    @GetMapping("/message")
+    public String getMessage(){
+        return "Hello from Spring boot";
+    }
+}
---
+++ src/main/java/com/example/Jocata/JocataApplication.java
@@ -0,0 +1,13 @@
+package com.example.Jocata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.*;
+
+@SpringBootApplication
+public class JocataApplication {
+	public static void main(String[] args) {
+		System.out.println("Hello from Spring Boot!");
+		SpringApplication.run(JocataApplication.class, args);
+	}
+}
